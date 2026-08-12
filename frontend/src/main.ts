957657cd884c7cbd2333e59742d3e4caf9695326
farukskya <beyomer013@gmail.com>
2026-08-12 14:30:38 +0300

    zone.js maints eklendi

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js'; // <-- NG0908 HATASINI ÇÖZEN KRİTİK SATIRS
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app';
