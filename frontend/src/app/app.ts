72bae77e56f7ffdfc61fb659230e35abb483266f
farukskya <beyomer013@gmail.com>
2026-08-13 09:33:54 +0300

    m

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -96,11 +96,6 @@ export class AppComponent implements OnInit {
 
   toggleDarkMode() {
     this.isDarkMode = !this.isDarkMode;
-    if (this.isDarkMode) {
-      document.body.classList.add('dark-theme');
-    } else {
-      document.body.classList.remove('dark-theme');
-    }
   }
 
   get filtrelenmisPersoneller(): Personel[] {
