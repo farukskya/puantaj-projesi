b7ddec95010c04823595a6f85d8a3a00a59b4301
farukskya <beyomer013@gmail.com>
2026-08-13 08:41:42 +0300

    arayuz butonları elden gecırıldı,aydınlık koyu mod kontrol edıldı,profılım sekmesı guncellendı

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -98,6 +98,11 @@ export class AppComponent implements OnInit {
 
   toggleDarkMode() {
     this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-theme');
+    } else {
+      document.body.classList.remove('dark-theme');
+    }
   }
 
   // Anlık Personel Filtreleyici
